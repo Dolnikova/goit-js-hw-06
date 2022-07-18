aa51a6cd870382170747af95e69b95cb9ea3476a
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-07-18 23:27:27 +0300

    enter

---
+++ src/src/js/task-01.js
@@ -0,0 +1,27 @@
+// class Item {
+//   #categories;
+//   constructor(categories) {
+//     this.categories = categories;
+//   }
+//   getCategories() {
+//     return this.categories;
+//   }
+//   countCategories(categories) {
+//     this.categories += this.categories;
+//   }
+// }
+// let numberOfCategories = 0;
+// for (category of categories) {
+//   numberOfCategories += li.item;
+// }
+// const navEl = document.querySelector("li");
+// const navLinkEl = navEl.querySelectorAll("li");
+// console.log("navLinkEl", navLinkEl);
+const textTitleEl = document.querySelector(".text");
+const textTitleAll = textTitleEl.querySelector(".item.text");
+// const textItemAll = textTitleAll.querySelectorAll(".item.text");
+console.log("textTitleEl", textTitleEl);
+console.log("Category:", textTitleEl.textContent);
+// console.log("Category:", textItemAll.textContent);
+// console.log("navLinkEl", navLinkEl);
+// console.log(countCategories);
