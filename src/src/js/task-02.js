c98e9c570008af2efd464bad577e093105ae700a
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-07-19 16:22:39 +0300

    завдання2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// const ulEl = document.querySelector("#ingredients");
+// // const list = document.createElement("ul");
+// // list.classList.add("item");
+// const ingredient = (options) => {
+//   return options.map((option) => {
+//     const list = document.createElement("ul");
+//     list.classList.add("item");
+//     const link = document.createElement("li");
+//     link.textContent = ingredients[];
+//     return options;
+//   });
+// };
+// const elements = ingredient(ingredients);
+// // ingredientsElements.append(...elements);
+// console.log(elements);
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach((ingredient) => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+});
+
+console.log(foodIngredients);
